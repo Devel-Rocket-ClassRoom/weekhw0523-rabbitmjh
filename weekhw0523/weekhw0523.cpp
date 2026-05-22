4e6232a6a89b07acf279fad8943c386dd30fc621
rabbitmjh <rabbitmjh@naver.com>
2026-05-23 01:27:02 +0900

    feat: Homework03_Run

--- weekhw0523/weekhw0523.cpp
+++ weekhw0523/weekhw0523.cpp
@@ -19,11 +19,51 @@ void Homework02_Run()
 		Sum += Num % 10;
 		Num /= 10;
 	}
-	cout << Sum << endl;
+	cout <<"각 자리 수의 합은 : " << Sum << endl;
+
+	return;
 }
 
 void Homework03_Run()
 {
+	srand(time(0));
+
+	int Money = 10000, Count = 1;
+
+	while (Money > 100)
+	{
+		cout << Count << "회차, 소지 금액: " << Money << endl;
+		cout << "베팅할 금액을 입력하세요, 최소 100 이상 베팅해야합니다 : ";
+		
+		int Bet;
+		cin >> Bet;
+		while (Bet < 100)
+		{
+			cout << "최소 100 이상 베팅해야합니다. 다시 입력해주세요: ";
+			cin >> Bet;
+		}
+		int Num1 = (rand() % 9) + 1, Num2 = (rand() % 9) + 1, Num3 = (rand() % 9) + 1;
+		cout << "결과: " << Num1 << " " << Num2 << " " << Num3 << endl;
+		if (Num1 == Num2 && Num1 == Num3)
+		{
+			if (Num1 == 7)
+			{
+				cout << "잭팟!!!!! 베팅 금액의 10000배 " << Bet * 10000 << "획득!!!!!" << endl;
+				Money += Bet * 10000;
+			}
+			else
+			{
+				cout << "같은 숫자 3개가 나왔습니다! 베팅 금액의 50배 " << Bet * 50 << "획득!" << endl;
+				Money += Bet * 50;
+			}
+		}
+		else
+			Money -= Bet;
+		Count++;
+	}
+	cout << "게임 오버" << endl;
+
+	return;
 }
 
 void Homework04_Run()
